448c6dbe8dbf1663f2cd91c145ee6ba8faf9fa00
cjp <1430417057@qq.com>
2024-03-04 18:40:13 +0800

    1

--- app.py
+++ app.py
@@ -3,11 +3,22 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+@app.route('/index')
+def show_index():
     # 这里可以根据实际情况填充台架、版本和航线的选项
     test_stands = ['K4.5', 'K5.0']
     routes = ['航线1', '航线2', '航线3']
     return render_template('index.html', test_stands=test_stands, routes=routes)
+
+#测试报告页面
+@app.route('/test_reports')
+def show_test_reports():
+    return render_template('test_reports.html')
+
+@app.route('/test_results')
+def show_test_results():
+    return render_template('test_results.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0',port=1234)
