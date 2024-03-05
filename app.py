1b461fd0cb2d13304a2f7ab972f1eee739f8b5cb
cjp <1430417057@qq.com>
2024-03-05 16:54:53 +0800

    1

--- app.py
+++ app.py
@@ -1,23 +1,45 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,send_file,jsonify,request
+import json
+import paramiko
 
 app = Flask(__name__)
+
+def load_config():
+    with open('config.json') as fp:
+        return json.load(fp)
+config=load_config()
 
 @app.route('/')
 @app.route('/index')
 def show_index():
-    # 这里可以根据实际情况填充台架、版本和航线的选项
-    test_stands = ['K4.5', 'K5.0']
-    routes = ['航线1', '航线2', '航线3']
-    return render_template('index.html', test_stands=test_stands, routes=routes)
+
+    test_stands=list(config.keys()) #台架列表
+    test_stand = request.args.get('test_stand') #点击按钮时选择的台架
+    version = request.args.get('version')   #点击按钮时输入的版本
+    #点击测试按钮后，如果符合条件，则开始测试
+    if test_stand=='K4.5' and version:
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(config[test_stand]["host"], username=config[test_stand]["username"], password=config[test_stand]["password"])
+        command="/home/daa/test_group/manifest/cicd/hil/hil_upgrade_and_test.sh 3843 3843"
+        stdin, stdout, stderr = ssh.exec_command(command)
+        output = stdout.readlines()
+        print(output)
+        ssh.close()
+
+    return render_template('index.html', test_stands=test_stands)
+
+#测试结果页面
+@app.route('/test_results')
+def show_test_results():
+    return render_template('test_results.html')
 
 #测试报告页面
 @app.route('/test_reports')
 def show_test_reports():
-    return render_template('test_reports.html')
+    #return render_template('test_reports.html')
+    return send_file('templates/03-04-1518.html')
 
-@app.route('/test_results')
-def show_test_results():
-    return render_template('test_results.html')
 
 
 if __name__ == '__main__':
--- static/js/app.js
+++ static/js/app.js
@@ -1,20 +1,34 @@
-// 按钮点击事件监听
-document.getElementById('start_test_btn').addEventListener('click', function() {
+function checkVersion(){
+    //开始测试按钮
+    var start_test_btn=document.getElementById('start_test_btn')
     // 获取输入框的值
     var version = document.getElementById('version').value;
-    // 正则表达式验证四位数
-    var versionRegex = /^\d{4}$/;
 
-    // 清除之前的错误消息
-    document.getElementById('error-msg').textContent = '';
+    // 正则表达式验证四位数
+    var versionRegex = /^\d{4,6}$/;
 
     // 判断版本号是否符合四位数
-    if(versionRegex.test(versionNumber)) {
+    if(versionRegex.test(version)) {
         // 版本号格式正确，可以进行后续操作
-        console.log("版本号正确，开始测试...");
+        //开始测试按钮置灰
+        //start_test_btn.disabled = true;
         // 这里可以添加开始测试的代码
+        return true
     } else {
         // 版本号格式不正确，显示错误消息
-        document.getElementById('error-msg').textContent = '请输入四位数版本号！';
+        alert("请输入正确的版本号！（四位数以上）");
+        return false
+    }
+}
+
+// 按钮点击事件监听
+//document.getElementById('start_test_btn').addEventListener('click', checkVersion);
+
+// 提交表单监听
+document.getElementById('test_form').addEventListener('submit',function(event){
+    result=checkVersion();
+    console.log(result);
+    if (result==false){
+        event.preventDefault(); //阻止表单提交
     }
 });
